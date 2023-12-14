0f2ca8e76b96abf5cb4902117b71352d75515c0a
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2023-12-14 17:19:47 -0600

    Construcción de promesa

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css';
 import javascriptLogo from './javascript.svg';
 import viteLogo from '/vite.svg';
-import { callbacks, demo } from './src/concepts';
+import { callbacks, demo, promises } from './src/concepts';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -20,4 +20,4 @@ document.querySelector('#app').innerHTML = `
 
 let appCard = document.querySelector(".card");
 
-callbacks(appCard);
+promises(appCard);
--- src/concepts/index.js
+++ src/concepts/index.js
@@ -3,3 +3,4 @@ export { demo } from "./demo";
 export { showEnv } from "./1_environmens";
 export { callbacks } from "./2_callbacks";
 export { heros } from "../data/heros";
+export { promises } from "./3_promises";
