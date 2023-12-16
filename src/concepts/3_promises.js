52078df23a2fa8e17868d5081b2d05f0b00711e7
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2023-12-15 19:51:00 -0600

    Promisse all

--- src/concepts/3_promises.js
+++ src/concepts/3_promises.js
@@ -9,6 +9,13 @@ export const promises = (element) => {
         element.innerText = hero.name;
     }
 
+    const showTwoHeros = (hero1, hero2) => {
+        element.innerHTML = `
+            <h3>${hero1.name}</h3>
+            <h3>${hero2.name}</h3>
+        `;
+    }
+
     const showError = (error) => {
         element.innerHTML = `
             <h1>Error:</h1>
@@ -17,10 +24,38 @@ export const promises = (element) => {
     }
 
     let heroId1 = '5d86371f1efebc31def272e2';
+    let heroId2 = '5d86371f233c9f2425f16916';
 
+    // let hero1;
+
+    //* Consumir forma base
+    /*
     findHero(heroId1)
         .then( hero => showHero(hero) )
         .catch( error => showError(error) );
+    */
+
+    //* Consumir anidado hell
+    /*
+    findHero( heroId1 )
+        .then( hero => {
+            hero1 = hero;
+            return findHero(heroId2);
+        }).then( hero2 => {
+            showTwoHeros(hero1, hero2);
+        }).catch( showError );
+    */
+
+    //* Consumir promise.all
+
+    Promise.all([
+        findHero(heroId1),
+        findHero(heroId2),
+    ])
+    .then( ([hero1, hero2]) => showTwoHeros(hero1, hero2) )
+    .catch( showError );
+
+
 }
 
 /**
