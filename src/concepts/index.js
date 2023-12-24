b2083705a62d0565a6a9ef97a4ef0d4acb1e6255
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2023-12-24 10:36:28 -0600

    Prueba

--- src/concepts/index.js
+++ src/concepts/index.js
@@ -1,12 +1,11 @@
-
 export { demo } from "./demo";
+export { asyncComponent } from "./5_async";
+export { asyncAwait } from "./6_async_await";
+export { asyncAwaitExample } from "./7_aync_await_example";
+export { asyncAwaitOptimize } from "./8_async_await_optimize";
 export { showEnv } from "./1_environmens";
 export { callbacks } from "./2_callbacks";
 export { heros } from "../data/heros";
 export { promises } from "./3_promises";
 export { promiseRase } from "./4_promise_rase";
-export { asyncComponent } from "./5_async";
-export { asyncAwait } from "./6_async_await";
-export { asyncAwaitExample } from "./7_aync_await_example";
-export { asyncAwaitOptimize } from "./8_async_await_optimize";
 export { forAwaitComponent } from "./9_for_if_await";
